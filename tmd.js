861e39610aef37ddff4889a07678a092779a1dfd
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2022-08-28 13:48:23 +0700

    add

--- tmd.js
+++ tmd.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 const express = require('express');
 const request = require('request');
 
@@ -46,13 +48,31 @@ app.get('/tmd',(req, res) => {
     let city = req.query.city;
     var request = require('request');
     
-    request(`https://samples.openweathermap.org/data/2.5/weather?q=London&mode=html&appid=6b84a8e0962dca9e3da7b398fe8d46f2`, function (error, response, htmlbody){
-        if (response.statusCode === 200) {
-            console.log(htmlbody)
-            //res.send(`${city}+${data.list[0].weather[0].description}`)
-            res.send(`${htmlbody}`)
-        } 
-    });
+    const url = new URL(URL_TMD_API_DAILY + '/at')
+    url.search = new URLSearchParams({
+        lat,
+        lon,
+        date,
+        duration,
+        hour,
+        fields: 'tc,tc_max,tc_min,rh,rain,cond,ws10m,wd10m',
+    })
+
+    try {
+        const result = await fetch(url, {
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + TMDTOKEN,
+        },
+        });
+        res.send(`${result}`)
+
+    } catch (error) {
+        throw error
+    }
+
+    
     
 });
 
